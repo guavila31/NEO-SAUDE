22cad575c2f96bfd1b0dd5f70a3761525e273b25
guavila31 <gustavoavila2000@hotmail.com>
2023-05-30 06:06:26 -0300

    configuracao para build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  iplocal: 'localhost:8080/api/',
+  ipprimario: 'localhost:8080/api/',
+  appToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndXN0YXZvLmF2aWxhQG5lb3NhdWRlLmNvbS5iciIsImlzcyI6IkFQSSBORU8gU0FVREUiLCJleHAiOjE2ODU1NjI2NDB9.iZl9re6Rvp1VQ_FaHCWJLXk_YIKSgxd9dJ7BmbCrMNA',
+  pathassets: '../../../../assets',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   iplocal: 'localhost:8080/api/',
   ipprimario: 'localhost:8080/api/',
   appToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndXN0YXZvLmF2aWxhQG5lb3NhdWRlLmNvbS5iciIsImlzcyI6IkFQSSBORU8gU0FVREUiLCJleHAiOjE2ODU1NjI2NDB9.iZl9re6Rvp1VQ_FaHCWJLXk_YIKSgxd9dJ7BmbCrMNA',
