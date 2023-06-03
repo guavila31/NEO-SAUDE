6db393df49e3f69005cfb25021a81285e872d49c
guavila31 <gustavoavila2000@hotmail.com>
2023-06-03 11:30:03 -0300

    Ajuste login

--- src/app/modals/modal-perfil/modal-perfil.component.ts
+++ src/app/modals/modal-perfil/modal-perfil.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ModalController, NavParams } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController, NavController, NavParams } from '@ionic/angular';
 import { PacienteInterface } from 'src/app/interface/paciente-interface';
 import { ApiService } from 'src/app/services/api-service.service';
 import { FormatadorDeDadosService } from 'src/app/services/formatador-de-dados.service';
@@ -10,27 +10,125 @@ import { FormatadorDeDadosService } from 'src/app/services/formatador-de-dados.s
   templateUrl: './modal-perfil.component.html',
   styleUrls: ['./modal-perfil.component.scss'],
 })
-export class ModalPerfilComponent  implements OnInit {
-
-  public sEmail: any = ''
+export class ModalPerfilComponent implements OnInit {
 
   private sIdPaciente: string = '1'
   public iDadosPaciente: PacienteInterface
+
+  public sNome: string | undefined
+  public sCpf: string | undefined
+  public sDataNasc: string | undefined
+  public sCelular: string | undefined
+  public sEmail: string | undefined
+
+  public iDadosPacienteAtualizar: PacienteInterface = {}
   constructor(
     private modalController: ModalController,
     private navParams: NavParams,
-    public formatador: FormatadorDeDadosService
+    public formatador: FormatadorDeDadosService,
+    private formBuilder: FormBuilder,
+    private api: ApiService,
+    private alertController: AlertController,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController
   ) {
     this.iDadosPaciente = this.navParams.get('iDadosPaciente');
+    this.sNome = this.iDadosPaciente.nome
+    this.sCpf = this.iDadosPaciente.cpf
+    this.sDataNasc = this.iDadosPaciente.dataNascimento
+    this.sCelular = this.iDadosPaciente.celular
+    this.sEmail = this.iDadosPaciente.email
     console.log('Paciente: ', this.iDadosPaciente)
-   }
+  }
 
   ngOnInit() {
   }
 
-  salvarPerfil(){}
+  formatarData(event: any) {
+    const valor = event.target.value.toString();
+    let dataFormatada = '';
+    if (valor.length === 2) {
+      dataFormatada = valor + '/';
+      this.sDataNasc = dataFormatada;
+    } else if (valor.length === 5) {
+      dataFormatada = valor.slice(0, 3) + '' + valor.slice(3) + '/';
+      this.sDataNasc = dataFormatada;
+    } else if (valor.length > 5) {
+      dataFormatada = valor.slice(0, 10);
+      this.sDataNasc = dataFormatada;
+    }
+    console.log('Data: ', this.sDataNasc)
+  }
 
-  sair(){
-    this.modalController.dismiss()
+  construirBody() {
+    this.iDadosPaciente = {
+      id: this.iDadosPaciente.id,
+      celular: this.sCelular,
+      cpf: this.sCpf,
+      dataNascimento: this.sDataNasc,
+      nome: this.sNome
+    }
+  }
+
+  async salvarPerfil() {
+    this.construirBody()
+    const LOADING = await this.loadingCtrl.create({ message: "Aguarde...", mode: 'ios' });
+    LOADING.present();
+    try {
+      await this.api.req('paciente', [], 'put', this.iDadosPaciente, true, false, false)
+        .then(data => {
+          console.log('Data: ', data)
+          this.alertPadrao('Sucesso!', 'Dados atualizados com sucesso.')
+          LOADING.dismiss()
+        });
+    } catch (err) {
+      await this.alertPadrao('Erro!', 'Ocorreu algum erro, tente novamente.')
+      LOADING.dismiss()
+      console.log(err)
+      throw err;
+    }
+  }
+
+  /**
+   * @param sModo 'F' só fecha | 'A' abre um alert pra confirmacao
+   */
+  async sair(sModo: string) {
+    if (sModo === 'A') {
+      this.modalController.dismiss()
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Atenção',
+        subHeader: 'Deseja realmente sair?',
+        mode: 'ios',
+        buttons: [
+          { text: 'Ok', handler: () => { this.navCtrl.navigateRoot('/'), this.modalController.dismiss() } },
+          { text: 'Cancelar', role: 'cancel' },
+        ]
+      });
+      alert.present();
+    }
+  }
+
+  async excluirConta() {
+    const alert = await this.alertController.create({
+      header: 'Atenção!',
+      subHeader: 'Deseja realmente excluir sua conta permanentemente?',
+      mode: 'ios',
+      buttons: [
+        { text: 'Ok', handler: () => { this.navCtrl.navigateRoot('/'), this.modalController.dismiss() } },
+        { text: 'Cancelar', role: 'cancel' },
+      ]
+    });
+    alert.present();
+  }
+
+  async alertPadrao(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      subHeader: mensagem,
+      mode: 'ios',
+      buttons: [{ text: 'Ok', handler: () => { this.sair('F'); } }]
+    });
+    alert.present();
   }
 }
--- src/app/public/inicio/inicio.page.ts
+++ src/app/public/inicio/inicio.page.ts
@@ -8,10 +8,28 @@ import { ApiService } from 'src/app/services/api-service.service';
 })
 export class InicioPage {
 
+  public sTipoUsuario: 'médico' | 'paciente' = 'médico'
+  public bEMedico: boolean = false
+  public bELogin: boolean = true
+
   constructor(private api: ApiService) {}
 
   registrar(){}
 
   entrar(){}
 
+  alterarTipoUsuario(){
+    this.bEMedico = !this.bEMedico
+
+    if(this.bEMedico)
+      this.sTipoUsuario = 'paciente'
+    else
+      this.sTipoUsuario = 'médico'
+    console.log('Usuario: ', this.bEMedico);
+  }
+
+  alterarTipoEntrada(){
+    this.bELogin = !this.bELogin
+    console.log('login: ', this.bELogin);
+  }
 }
